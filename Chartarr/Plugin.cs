b0f248950e68760fa2e5ce5daf344950f8437127
alperien <emailadrss@proton.me>
2026-07-23 20:11:52 +0500

    plugin entry point

---
+++ Chartarr/Plugin.cs
@@ -0,0 +1,11 @@
+using NzbDrone.Core.Plugins;
+
+namespace Chartarr
+{
+    public class ChartarrPlugin : Plugin
+    {
+        public override string Name => "Chartarr";
+        public override string Owner => "alperien";
+        public override string GithubUrl => "https://github.com/alperien/chartarr.plugin";
+    }
+}
